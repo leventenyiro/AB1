4d33593f8bb247abb718eae74f3a866727d0b23e
Levente Nyírő <nyiro.levente@gmail.com>
2022-04-07 09:45:48 +0200

    Update task9.sql

--- task9.sql
+++ task9.sql
@@ -26,3 +26,31 @@ natural join vzoli.osztaly
 where get_foglalkozas(onev) <> 'NULL';
 
 -- https://people.inf.elte.hu/vzoli/Adatbazisok_1/PLSQL/
+
+-- cursors
+
+set serveroutput on
+declare
+    cursor c_curs1(oa number) is select oazon, dnev from dolgozo where oazon = oa;
+    rec c_curs1%rowtype;
+begin
+    open c_curs1(10);
+    loop
+        fetch c_curs1 into rec;
+        exit when c_curs1%notfound; -- ha nem található, akkor off
+        dbms_output.put_line(to_char(rec.oazon) || ' - ' || rec.dnev);
+    end loop;
+    close c_curs1;
+end;
+
+-- zh: készítsük el azt a tárolt eljárást (procedure)
+    -- ilyenkor nem lehet selecttel helyettesíteni
+    
+-- zh
+    -- egymásba ágyazott 2 kurzor
+    -- példa pl_03_kurzor.txt
+    
+-- with
+    -- ki szállítja a legtöbb cikket
+    -- meg kell nézni ki mit szállít, és meg kell nézni a maxot (2 lépcsős mechanizmu
+    -- count distinct kod
