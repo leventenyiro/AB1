bc51ad02cfe382ba59b4b134b9f62280006b8b20
Nyírő Levente Gyula <b4kzan@inf.elte.hu>
2022-05-11 20:29:06 +0200

    10.1 is ready

--- zh2/feladat10.sql
+++ zh2/feladat10.sql
@@ -11,27 +11,17 @@ CREATE OR REPLACE PROCEDURE fiz_mod IS
     cursor c1 is select min(dkod) as min_dkod, oazon, min(belepes) as min_belepes, (select count(*) from dolgozo where oazon = d1.oazon) - 1 as rajta_kivul
         from dolgozo d1
         group by oazon;
-    --rec c1%rowtype;
 begin
-    /*open c1;
-    loop
-        fetch c1 into rec;
-        update dolgozo set fizetes = fizetes + rec.rajta_kivul * 100 where dkod = rec.min_dkod;
-        exit when c1%notfound;
-        for item in (select dkod, fizetes, oazon from dolgozo where dkod = rec.min_dkod) loop
-            dbms_output.put_line(item.dkod || ' ' || item.fizetes);
-        end loop;
-
-    end loop;
-    close c1;*/
     for sor in c1 loop
         update dolgozo set fizetes = fizetes + sor.rajta_kivul * 100 where dkod = sor.min_dkod;
     end loop;
     
+    for item in (select round(avg(fizetes), 2) as atl from dolgozo) loop
+        dbms_output.put_line(item.atl);
+    end loop;
+    
     rollback;
 end;
-
--- még nem jó
 
 set serveroutput on
 execute fiz_mod();
